a9034e1d455c7233f326957b00aecea59d6e5ca0
Filippo Biondi <f.biondi12@studenti.unipi.it>
2023-11-03 19:15:58 +0100

    Functioning implementation of drag and drop and resizable plots

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,56 +1,89 @@
-<script setup lang="ts">
-import { reactive } from 'vue'
-
-const layout = reactive([
-  { x: 0, y: 0, w: 2, h: 2, i: '0' },
-  { x: 2, y: 0, w: 2, h: 4, i: '1' }
-])
-const layout2 = reactive([
-  { x: 0, y: 0, w: 2, h: 2, i: '0' },
-  { x: 2, y: 0, w: 2, h: 4, i: '1' },
-  { x: 4, y: 0, w: 2, h: 2, i: '2' }
-])
-</script>
-
 <template>
     <v-container class="rounded-xl px-16 my-8 pb-10 bg-white">
-    <v-responsive class="fill-height" style="overflow: visible !important">
-  <div style="margin-top: 10px">
-    <h4>Grid #1</h4>
-    <GridLayout v-model:layout="layout" :row-height="30">
-      <template #item="{ item }">
-        <span class="text">{{ item.i }}</span>
-      </template>
-    </GridLayout>
-  </div>
-  <div style="margin-top: 10px">
-    <h4>Grid #2</h4>
-    <GridLayout :layout="layout2" :row-height="30">
-      <template #item="{ item }">
-        <span class="text">{{ item.i }}</span>
-      </template>
-    </GridLayout>
-  </div>
-    </v-responsive>
+        <v-switch v-model="resizable"/> Resizable
+        <v-switch v-model="responsive"/> Responsive
+        <v-switch v-model="draggable" :false-value="true" :true-value="false" :label="smart_switch_tex"/>
+        <br />
+        <div style="margin-top: 10px">
+          <GridLayout
+            v-model:layout="layout"
+            :col-num="4"
+            :cols = '{ lg: 4, md: 4, sm: 2, xs: 1, xxs: 1 }'
+            :row-height="250"
+            :is-draggable="draggable"
+            :is-resizable="resizable"
+            :responsive="responsive"
+          >
+            <template #item="{ item }">
+              <DashboardElement
+                :chart-configuration="test_chart"
+              />
+            </template>
+          </GridLayout>
+        </div>
     </v-container>
 </template>
 
+<script lang="ts">
+import DashboardElement from "@/components/DashboardElement.vue";
+import {ref, reactive, defineComponent} from 'vue'
+import {ChartConfiguration} from "chart.js";
+
+export default defineComponent({
+  name: "Dashboard",
+  props: {},
+  data() {
+    return {
+      draggable: true,
+      resizable: true,
+      responsive: true,
+      layout: reactive([
+        { x: 0, y: 0, w: 2, h: 1, i: '0' },
+        { x: 2, y: 0, w: 2, h: 1, i: '1' },
+        { x: 0, y: 2, w: 2, h: 1, i: '2' },
+        { x: 2, y: 2, w: 2, h: 1, i: '3' },
+      ]),
+      test_chart: {
+        type: 'bar',
+        data: {
+          labels: ['January', 'February', 'March'],
+          datasets: [{ data: [40, 20, 12],
+                       backgroundColor: ["red", "green", "blue"],}]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      } as ChartConfiguration,
+    //   can be improved, but I don't know how to use typescript
+    };
+  },
+  methods: {},
+  computed: {
+    smart_switch_tex() {
+      return this.draggable ? "Custom" : "Smart";
+    }
+  },
+  components: { DashboardElement },
+});
+
+</script>
+
 <style scoped>
 .vgl-layout {
-  background-color: #eee;
+  background-color: white;
 }
 
 :deep(.vgl-item:not(.vgl-item--placeholder)) {
-  background-color: #ccc;
   border: 1px solid black;
 }
 
 :deep(.vgl-item--resizing) {
-  opacity: 90%;
+  opacity: 100%;
 }
 
 :deep(.vgl-item--static) {
-  background-color: #cce;
+  background-color: white;
 }
 
 .text {
