e1565189a1280fe506625ad09b1dc0f80ae6c4ab
PaulMagos <p.magos@studenti.unipi.com>
2023-11-05 17:04:24 +0100

    Update to favicon

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,10 @@
       <Navbar>
       </Navbar>
       <v-main class="bg-grey-lighten-5">
-            <v-container class="rounded-xl px-16 my-8 pb-10 bg-white">
+            <v-container class="rounded-xl px-12 py-6 my-8 pb-10 bg-white">
               <router-view></router-view>
             </v-container>
             <ChatBot/>
-            <!-- <v-btn class="chat-btn" icon="mdi-robot" size="x-large" @click=new URL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")></v-btn> -->
       </v-main>
   </v-app>
 </template>
