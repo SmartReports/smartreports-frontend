96e4de74a743eac838a4d8560c0a55ca55cf23c3
Simone Ianniciello <iannisimo@gmail.com>
2023-11-07 16:10:42 +0100

    add close button

--- src/components/ChatBot.vue
+++ src/components/ChatBot.vue
@@ -2,18 +2,41 @@
   <v-btn class="chat-btn d-none d-lg-block" icon="mdi-robot" size="x-large" @click="btnClick"></v-btn>
   <v-btn class="chat-btn d-none d-md-block d-lg-none" icon="mdi-robot" size="large" @click="btnClick"></v-btn>
   <v-btn class="chat-btn d-block d-md-none d-lg-none" icon="mdi-robot" @click="btnClick"></v-btn>
-  <!-- open an iframe -->
-  <iframe id="chatbot" class="chatbot" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <div class="chatbot-container" id="chatbot-container">
+    <v-btn
+          text="Close"
+          icon="mdi-close"
+          color="red"
+          class="chatbot-close"
+          @click="closeChatbot"
+          ></v-btn>
+    <iframe id="chatbot-iframe" class="chatbot-iframe" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
 </template>
 
 <script lang="ts">
   export default {
+      mounted() {
+      },
       methods : {
           btnClick() {
-              const chatbot = document.getElementById("chatbot");
+              const chatbot = document.getElementById("chatbot-iframe");
+              const chatbotContainer = document.getElementById("chatbot-container");
+              if (chatbotContainer) {
+                chatbotContainer.style.display = "block";
+              }
               if (chatbot) {
-                chatbot.style.display = "block";
                 chatbot.setAttribute('src', "https://www.youtube.com/embed/dQw4w9WgXcQ?si=R6HpR5Bo7N2drZxI&autoplay=1")
+              }
+          },
+          closeChatbot() {
+              const chatbot = document.getElementById("chatbot-iframe");
+              const chatbotContainer = document.getElementById("chatbot-container");
+              if (chatbotContainer) {
+                chatbotContainer.style.display = "none";
+              }
+              if (chatbot) {
+                chatbot.setAttribute('src', "")
               }
           }
       }
@@ -27,13 +50,26 @@
   bottom: 30px;
 }
 
-.chatbot {
+.chatbot-iframe {
+  width: 100%;
+  height: 100%;
+}
+
+.chatbot-container {
   position: fixed;
   right: 30px;
   bottom: 30px;
   width: 40em;
   aspect-ratio: 5/4;
   display: none;
-
 }
+
+.chatbot-close {
+  position: absolute;
+  top: -1em;
+  right: -1em;
+  width: 1em;
+  height: 1em;
+}
+
 </style>
