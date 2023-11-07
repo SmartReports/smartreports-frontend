3e6478194633834e6a0a5e58cc38b812cb86f875
PaulMagos <p.magos@studenti.unipi.com>
2023-11-07 18:17:17 +0100

    Update to api repo

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,9 @@ import { GridLayout, GridItem } from "grid-layout-plus";
 const app = createApp(App);
 
 const dev = process.env.NODE_ENV !== "production";
-axios.defaults.baseURL = dev
-  ? process.env.VUE_APP_AXIOS_BASE ?? "http://127.0.0.1:8000"
-  : process.env.VUE_APP_AXIOS_BASE;
+axios.defaults.baseURL = dev ? process.env.VUE_APP_AXIOS_BASE ??
+"http://127.0.0.1:8000" :
+"https://paulmagos.pythonanywhere.com/smartreport_app/";
 
 registerPlugins(app);
 
