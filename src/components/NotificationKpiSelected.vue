4143e3718d2b83996914ba715162e50191b55e25
PaulMagos <p.magos@studenti.unipi.com>
2023-11-08 21:51:51 +0100

    Integrate be with alarms

--- src/components/NotificationKpiSelected.vue
+++ src/components/NotificationKpiSelected.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-row>
+    <v-row >
             <v-col cols="12" md="4">
-              <v-card-item>{{ KPIName }}</v-card-item>
+              <v-card-item>{{ getKpiName(KPIId) }}</v-card-item>
             </v-col>
             <v-col cols="3" md="2" sm="3" xs="2">
               <v-card-item>Min: {{ KPIMin }}</v-card-item>
@@ -10,7 +10,7 @@
               <v-card-item>Max: {{ KPIMax }}</v-card-item>
             </v-col>
             <v-col cols="3" md="2" sm="3" xs="1">
-              <v-btn color="green" height="40" width="100%">
+              <v-btn @click="$emit('update', 1, 4)" color="green" height="40" width="100%">
               <v-icon>
                 mdi-pencil
               </v-icon>
@@ -29,26 +29,33 @@
 
 
 <script lang="ts">
+import { PropType } from 'vue';
+import { useMainStore } from "../store/app";
+import { mapStores } from "pinia";
+import { emit } from 'process';
+import NotificationBell from "NotificationBell.vue"
 export default {
-  data() {
-    return {
-      min: 1,
-      max: 2,
-    }
+  methods: {
+    getKpiName(index: string) {
+        return this.mainStore.getKpiById(index)?.name
+    },
+  },
+  computed: {
+    ...mapStores(useMainStore),
   },
   props: {
+    KPIId: {
+      type: String as PropType<string>,
+      required: true,
+    },
     KPIMin: {
-      type: Number,
+      type: Number as PropType<number>,
       required: true,
     },
     KPIMax: {
-      type: Number,
-      required: true,
-    },
-    KPIName: {
-      type: String,
+      type: Number as PropType<number>,
       required: true,
     }
-  }
+  },
 }
 </script>
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from "./kpi";
+export * from "./alarms"
 export * from "./reports";
 export * from "./chartTypes"
