d35d556dfcdee08c1213c7a2563869d54cec209f
PaulMagos <p.magos@studenti.unipi.com>
2023-11-09 18:35:07 +0100

    Preferred View

--- src/components/TemplateRender/TemplateRender.vue
+++ src/components/TemplateRender/TemplateRender.vue
@@ -4,24 +4,34 @@
     <v-divider></v-divider>
     <v-container>
 
-    <v-carousel
-    cycle
-    progress="primary"
-    hide-delimiter-background
-    show-arrows="hover"
-  >
-    <v-carousel-item
-      v-for="(slide, i) in pages"
-      :key="i"
-    >
+      <v-card theme="dark" flat rounded="0">
+    <v-window v-model="onboarding">
+      <v-window-item v-for="n in pages" :key="`card-${n}`" :value="n">
+        <v-card height="200" class="d-flex justify-center align-center">
+          <span class="text-h2"> Card {{ n }} </span>
+        </v-card>
+      </v-window-item>
+    </v-window>
 
-      <template>
-        <DashboardElement
-        />
-        <!-- :chart-configuration="item.chart" -->
-      </template>
-    </v-carousel-item>
-  </v-carousel>
+    <v-card-actions class="justify-space-between">
+      <v-btn variant="plain" icon="mdi-chevron-left" @click="prev"></v-btn>
+      <v-item-group v-model="onboarding" class="text-center" mandatory>
+        <v-item
+          v-for="n in pages"
+          :key="`btn-${n}`"
+          v-slot="{ isSelected, toggle }"
+          :value="n"
+        >
+          <v-btn
+            :variant="isSelected ? 'outlined' : 'text'"
+            icon="mdi-record"
+            @click="toggle"
+          ></v-btn>
+        </v-item>
+      </v-item-group>
+      <v-btn variant="plain" icon="mdi-chevron-right" @click="next"></v-btn>
+    </v-card-actions>
+  </v-card>
 </v-container>
 
   </v-container>
@@ -31,11 +41,23 @@
 import { defineComponent, PropType } from 'vue';
 import { ReportTemplate } from "@/models"
 
-  export default {
+  export default defineComponent({
     data() {
       return {
         pages: 3,
+      onboarding: 1,
+
       }
+    },
+    methods: {
+      next() {
+        this.onboarding =
+          this.onboarding + 1 > this.pages ? 1 : this.onboarding + 1
+      },
+      prev() {
+        this.onboarding =
+          this.onboarding - 1 <= 0 ? this.pages : this.onboarding - 1
+      },
     },
     props: {
       modelValue: {
@@ -43,5 +65,5 @@ import { ReportTemplate } from "@/models"
         required: true,
       }
     }
-  }
+  })
 </script>
