c8c2cc359a0ed0b9f772125b3f79d957e6f95280
PaulMagos <p.magos@studenti.unipi.com>
2023-11-09 19:53:36 +0100

    FIX BECAUSE FIL IS DOWN

--- src/store/app.ts
+++ src/store/app.ts
@@ -30,39 +30,7 @@ export const useMainStore = defineStore("main", {
   },
   actions: {
     async getKpi() {
-      // this.kpi = (await axios.get("/kpi-list/")).data
-      this.kpi = [
-        {
-          id: "1",
-          name: "Production Cycle Time",
-          description: "KPI 1 description",
-          allowed_charts: ["bar", "line"],
-        },
-        {
-          id: "2",
-          name: "Energy Efficiency",
-          description: "KPI 2 description",
-          allowed_charts: ["bar", "line", "pie"],
-        },
-        {
-          id: "3",
-          name: "Maintenance Downtime vs. Equipment Age",
-          description: "KPI 3 description",
-          allowed_charts: ["scatter"],
-        },
-        {
-          id: "4",
-          name: "Quality First Pass Yield",
-          description: "KPI 4 description",
-          allowed_charts: ["bar", "line", "pie", "scatter"],
-        },
-        {
-          id: "5",
-          name: "Overall Equipment Effectiveness",
-          description: "KPI 5 description",
-          allowed_charts: ["bar", "line"],
-        },
-      ];
+      this.kpi = (await axios.get("/kpi-list/")).data
     },
     async getAlarms(accountId: any) {
       this.alarms = (await axios.get(`/alarms-list/?user_type=${accountId}`)).data
