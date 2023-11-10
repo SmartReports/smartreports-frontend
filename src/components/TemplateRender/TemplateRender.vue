3e9c4173414a37d6bb5e6a5b2150714a69447d94
PaulMagos <p.magos@studenti.unipi.com>
2023-11-10 23:17:33 +0100

    Implemented

--- src/components/TemplateRender/TemplateRender.vue
+++ src/components/TemplateRender/TemplateRender.vue
@@ -1,69 +1,91 @@
 <template>
-  <v-container>
-    <h3 class="text-h4 mt-4">Report {{   }}</h3>
-    <v-divider></v-divider>
-    <v-container>
+  <v-dialog width="2100" height="500">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          class="text-grey-darken-1"
+          icon="mdi-eye"
+          variant="text"
+          tooltip="View"
+          v-bind="props"
+        />
+      </template>
+      <template v-slot:default="{ isActive }">
+          <v-container>
+            <h3 class="text-h4 mt-4">{{ modelValue.name }}</h3>
+            <v-divider></v-divider>
+            <v-container>
 
-      <v-card theme="dark" flat rounded="0">
-    <v-window v-model="onboarding">
-      <v-window-item v-for="n in pages" :key="`card-${n}`" :value="n">
-        <v-card height="200" class="d-flex justify-center align-center">
-          <span class="text-h2"> Card {{ n }} </span>
-        </v-card>
-      </v-window-item>
-    </v-window>
+            <v-card theme="dark" flat rounded="10">
+            <v-window v-model="onboarding">
+              <v-window-item v-for="(page, i) in pages" :key="`card-${i}`" :value="i">
+                <v-card height=100 class="d-flex justify-center align-center">
+                    <template-render-page :user_type="user_type" :modelPage="page"></template-render-page>
+                </v-card>
+              </v-window-item>
+            </v-window>
 
-    <v-card-actions class="justify-space-between">
-      <v-btn variant="plain" icon="mdi-chevron-left" @click="prev"></v-btn>
-      <v-item-group v-model="onboarding" class="text-center" mandatory>
-        <v-item
-          v-for="n in pages"
-          :key="`btn-${n}`"
-          v-slot="{ isSelected, toggle }"
-          :value="n"
-        >
-          <v-btn
-            :variant="isSelected ? 'outlined' : 'text'"
-            icon="mdi-record"
-            @click="toggle"
-          ></v-btn>
-        </v-item>
-      </v-item-group>
-      <v-btn variant="plain" icon="mdi-chevron-right" @click="next"></v-btn>
-    </v-card-actions>
-  </v-card>
-</v-container>
-
-  </v-container>
+            <v-card-actions class="justify-space-between">
+              <v-btn :disabled="onboarding<=1" variant="plain" icon="mdi-chevron-left" @click="prev"></v-btn>
+              <v-item-group v-model="onboarding" class="text-center" mandatory>
+                <v-item
+                  v-for="(page, i) in pages"
+                  :key="`btn-${i}`"
+                  v-slot="{ isSelected, toggle }"
+                  :value="i"
+                >
+                  <v-btn
+                    :variant="isSelected ? 'outlined' : 'text'"
+                    icon="mdi-record"
+                    @click="toggle"
+                  ></v-btn>
+                </v-item>
+              </v-item-group>
+              <v-btn :disabled="onboarding==pages.length-1" variant="plain" icon="mdi-chevron-right" @click="next"></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-container>
+      </v-container>
+</template>
+</v-dialog>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
-import { ReportTemplate } from "@/models"
+import { ReportTemplate, ReportTemplatePage } from "@/models"
+import TemplateRenderPage from './TemplateRenderPage.vue';
 
   export default defineComponent({
     data() {
-      return {
-        pages: 3,
-      onboarding: 1,
-
-      }
+        return {
+            pages: this.modelValue.pages,
+            onboarding: 1,
+        };
     },
     methods: {
-      next() {
-        this.onboarding =
-          this.onboarding + 1 > this.pages ? 1 : this.onboarding + 1
-      },
-      prev() {
-        this.onboarding =
-          this.onboarding - 1 <= 0 ? this.pages : this.onboarding - 1
-      },
+        next() {
+            console.log(this.onboarding>1)
+            this.onboarding =
+                this.onboarding + 1 > this.pages.length ? 1 : this.onboarding + 1;
+
+        },
+        prev() {
+            console.log(this.onboarding)
+            this.onboarding =
+                this.onboarding - 1 < 1 ? this.pages.length : this.onboarding - 1;
+
+                console.log(this.onboarding)
+              },
     },
     props: {
-      modelValue: {
-        type: Object as PropType<ReportTemplate>,
-        required: true,
-      }
-    }
-  })
+        modelValue: {
+            type: Object as PropType<ReportTemplate>,
+            required: true,
+        },
+        user_type: {
+            type: String as PropType<string>,
+            required: true,
+        }
+    },
+    components: { TemplateRenderPage }
+})
 </script>
---
+++ src/components/TemplateRender/TemplateRenderPage.vue
@@ -0,0 +1,75 @@
+<template>
+    <v-container>
+        <v-row v-for="row in rows">
+            <v-col v-for="col in cols">
+                <DashboardElement :chart-configuration="getNext"></DashboardElement>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { ReportTemplatePage, KpiReportElement, ChartType } from "@/models"
+import DashboardElement from '../DashboardElement.vue';
+import {ChartConfiguration } from "chart.js"
+export default {
+    data() {
+        return {
+          numOfCharts: 0,
+          rows: 0,
+          pageKpis: [] as KpiReportElement[],
+          counter: 0,
+          cols: 0,
+          default_chart_options: {
+            responsive: true,
+            maintainAspectRatio: false
+          },
+          chart_data: {} as { [key: string]: ChartConfiguration },
+        }
+    },
+    props: {
+        user_type: {
+          type: String as PropType<String>,
+          required: true,
+        },
+        modelPage: {
+          type: Object as PropType<ReportTemplatePage>,
+          required: true,
+        }
+    },
+    created() {
+      if (this.modelPage.layout == 'grid'){
+        this.rows = (this.modelPage.elements.length)/2;
+        this.cols = 2;
+      }else {
+        if (this.modelPage.layout == 'horizontal'){
+          this.rows = 1;
+          this.cols = this.modelPage.layout.length;
+        }else{
+          if (this.modelPage.layout == 'vertical')
+            this.rows = this.modelPage.layout.length;
+            this.cols = 1;
+        }
+      }
+      this.numOfCharts = this.modelPage.elements.length;
+      this.pageKpis = this.modelPage.elements;
+      this.pageKpis.forEach(async (kpi) => {
+        const type = kpi.chart_type as ChartType
+        const kpi_id = kpi.kpi as string
+        this.chart_data[kpi_id] = {
+          data: (await this.axios.get(`/kpi-data/?user_type=${this.user_type}&chart_type=${type}&kpi_id=${kpi_id}`)).data["data"],
+          options: this.default_chart_options,
+          type: type
+        } as ChartConfiguration
+      })
+    },
+    methods: {
+      getNext() {
+        return this.chart_data[this.counter++];
+      }
+    },
+    computed: {},
+    components: { DashboardElement }
+}
+</script>
