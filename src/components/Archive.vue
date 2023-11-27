75bf3b104ac095aa8a760b3391f8397589ed7d3e
Anette Johansen <evaajo@stud.ntnu.no>
2023-11-27 18:23:56 +0100

    connect to backend

--- src/components/Archive.vue
+++ src/components/Archive.vue
@@ -23,19 +23,19 @@
         </v-container>
         <v-divider></v-divider>
         <v-container>
-          <v-list-item v-for="report in reports" :key="report.key">
+          <v-list-item v-for="report in reports" :key="report.id">
             <v-row>
               <v-col cols="3" md="3" sm="3">
-                <v-card-subtitle class="text-h6">{{ report.name }}</v-card-subtitle>
+                <v-card-subtitle class="text-h6">{{ getName(report.id) }}</v-card-subtitle>
               </v-col>
               <v-col cols="3" md="3" sm="3">
-                <v-card-subtitle class="text-h6">{{ report.date }}</v-card-subtitle>
+                <v-card-subtitle class="text-h6">{{ report.created }}</v-card-subtitle>
               </v-col>
               <v-col cols="3" md="3" sm="3">
-                <v-card-subtitle class="text-h6">{{ report.freq }} report</v-card-subtitle>
+                <v-card-subtitle class="text-h6">{{ getFrequency(report.id) }}</v-card-subtitle>
               </v-col>
               <v-col cols="3" md="3" sm="3">
-                <v-btn height="30" width="90%"><v-icon>mdi-download</v-icon></v-btn>
+                <v-btn @click="downloadReport(report.file)" height="30" width="90%"><v-icon>mdi-download</v-icon></v-btn>
               </v-col>
               <v-divider></v-divider>
             </v-row>
@@ -46,19 +46,86 @@
   </v-container>
 </template>
 
-
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
+import { useMainStore } from "../store/app";
+import axios from "axios";
+import { format } from 'date-fns';
+import { ReportTemplate } from "@/models";
+
+interface Report {
+  id: number;
+  created: string;
+  file: string;
+  user_type: string;
+}
 
 export default defineComponent({
+  props: {
+    user_type: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
   data() {
     return {
-      reports: [
-        { key: 1, name: "Report Name 1", date: "01.04.2023", freq: "Weekly" },
-        { key: 2, name: "Report Name 2", date: "08.04.2023", freq: "Yearly" },
-        { key: 3, name: "Report Name 3", date: "15.04.2023", freq: "Monthly" },
-      ],
+      reports: [] as Report[],
+      templates: [] as ReportTemplate[],
+
     };
+  },
+  methods: {
+    async fetchReports() {
+      try {
+        console.log('Fetching reports for user_type:', this.user_type);
+        const response = await axios.get("/report-archive/");
+        console.log('Response:', response.data);
+
+        const reports = response.data.map((report: any) => ({
+          ...report,
+          created: format(new Date(report.created), 'yyyy-MM-dd HH:mm:ss'),
+        })) as Report[];
+
+        // Filter reports based on user_type
+        this.reports = reports.filter(report => report.user_type === this.user_type);
+        console.log('Filtered Reports:', this.reports);
+      } catch (error) {
+        console.error("Error fetching reports:", error);
+      }
+    },
+
+    downloadReport(file: string) {
+      window.open(file, '_blank');
+    },
+
+    async fetchReportTemplates() {
+      try {
+        const response = await axios.get("/report-templates/");
+        this.templates = response.data;
+      } catch (error) {
+        console.error("Error fetching report templates:", error);
+      }
+    },
+
+    getFrequency(reportId: number): string {
+  const template = this.templates.find((t) => t.id === reportId);
+  return template ? template.frequency : 'N/A';
+},
+
+getName(reportId: number): string {
+  const template = this.templates.find((t) => t.id === reportId);
+  return template ? template.name : 'N/A';
+},
+
+
+    
+  },
+  watch: {
+    user_type: 'fetchReports', // Watch for changes in user_type and call fetchReports
+  },
+  mounted() {
+    console.log('Component mounted.');
+    this.fetchReports();
   },
 });
 </script>
